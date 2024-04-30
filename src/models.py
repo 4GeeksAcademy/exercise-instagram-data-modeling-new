916aa2bd8b1aa5d6254b303cf03bb26414ea6f05
Michel Rivero <157402536+michelriv@users.noreply.github.com>
2024-04-30 19:34:30 +0000

    e

--- src/models.py
+++ src/models.py
@@ -4,31 +4,47 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
-
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class User(Base):
+    __tablename__ = 'user'
+    ID = Column(Integer, primary_key=True)
+    User_ID = Column(Integer, ForeignKey('follower.User_from_ID'))
+    Username = Column(String(20), ForeignKey('user.Username'))
+    Firstname = Column(String(30))
+    Lastname = Column(String(30))
+    Email = Column(String, unique=True)
+    Password = Column(String(20))
+    Phone = Column(Integer)
+
+
+class Post(Base):
+    __tablename__ = 'post'
+    ID = Column(Integer, primary_key=True)
+    User_id = Column(Integer, ForeignKey('user.User_ID'))
+    Message = Column(String)
+    Created_at = Column(Integer)
+
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    ID = Column(Integer, primary_key=True)
+    Author_ID = Column(Integer, ForeignKey('user.User_ID'))
+    Content = Column(String)
+    post_id = Column(Integer, ForeignKey('post.ID'))
+
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    User_from_ID = Column(Integer, primary_key=True)
+    User_to_ID = Column(Integer, ForeignKey('user.User_ID'))
 
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
+
+# Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
